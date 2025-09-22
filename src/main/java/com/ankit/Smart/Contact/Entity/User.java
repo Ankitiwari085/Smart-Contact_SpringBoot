18299436b6f91fcf7c4c4bdd08a0e164d5f78504
Ankitiwari085 <ankitiwari085@gmail.com>
2025-09-22 18:35:41 +0530

    Initial commit

---
+++ src/main/java/com/ankit/Smart/Contact/Entity/User.java
@@ -0,0 +1,116 @@
+package com.ankit.Smart.Contact.Entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int Id;
+    private String name;
+    @Column(unique = true)
+    private  String email;
+    private String password;
+    @Column(length = 500)
+    private String about;
+    private String role;
+    private boolean enabled;
+    private String imageUrl;
+
+    @OneToMany(cascade = CascadeType.ALL ,fetch = FetchType.LAZY , mappedBy = "user")
+    private List<Contact> contactList=new ArrayList<>();
+
+    public User() {
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public List<Contact> getContactList() {
+        return contactList;
+    }
+
+    public void setContactList(List<Contact> contactList) {
+        this.contactList = contactList;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "Id=" + Id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", about='" + about + '\'' +
+                ", role='" + role + '\'' +
+                ", enabled=" + enabled +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", contactList=" + contactList +
+                '}';
+    }
+}
+
